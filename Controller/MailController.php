efbb2212f1fc1436647860d3e6876b5bf8865352
Fanny Pourtier (fannywho) <pourtanny@gmail.com>
2018-06-13 14:33:54 +0200

    add user method and send mail

---
+++ Controller/MailController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Controller;
+
+use Helper\Controller\BaseController as BaseController;
+
+class MailController extends BaseController
+{
+
+    public function sendMail($body, $to = 'press.start.dummy@gmail.com')
+    {
+        $transport = (new \Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
+            ->setUsername('press.start.dummy@gmail.com')
+            ->setPassword('lemdp123');
+
+        $mailer = new \Swift_Mailer($transport);
+
+        $message = (new \Swift_Message('Nouveau compte créé sur Press Start :)'))
+            ->setFrom(['press.start.dummy@gmail.com' => 'Press Start'])
+            ->setTo([$to => 'user'])
+            ->setBody($body,'text/html');
+
+        $mailer->send($message);
+    }
+}
